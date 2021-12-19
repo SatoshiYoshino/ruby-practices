b01c630006745181f3f98d069034e53a1a4bab50
Satoshi Yoshino <waterhanmer@gmail.com>
2021-12-19 16:02:48 +0900

    インデント修正

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,23 +1,23 @@
 
 def fizz_buzz(n)
-    n.to_s
+  n.to_s
 end
 
 def fizz_buzz(n)
-    if n % 15 == 0
-        'Fizz Buzz'
-    elsif n % 3 == 0
-        'Fizz'
-    elsif n % 5 == 0
-        'Buzz'
-    else
-        n.to_s
-    end
+  if n % 15 == 0
+    'Fizz Buzz'
+  elsif n % 3 == 0
+    'Fizz'
+  elsif n % 5 == 0
+    'Buzz'
+  else
+    n.to_s
+  end
 end
 
 range = 1..20
 range.each do |x|
-    puts fizz_buzz(x)
+  puts fizz_buzz(x)
 end
 
 
